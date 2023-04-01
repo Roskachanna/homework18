141f4e7786ef9f2b9db1c82e7f9058f7a376f860
annaroskach <danuta_tyt@mail.ru>
2023-04-01 17:01:53 +0300

    homework18

---
+++ src/exception/WrongLoginException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class WrongLoginException extends RuntimeException {
+
+
+    public WrongLoginException(String  errorMessage) {
+        super(errorMessage);
+    }
+}
---
+++ src/exception/WrongPasswordException.java
@@ -0,0 +1,6 @@
+package exception;
+
+public class WrongPasswordException extends RuntimeException  {
+    public WrongPasswordException(String errorMessage) {
+    }
+}
